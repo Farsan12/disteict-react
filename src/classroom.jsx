b964d31355cf3d9aea74434de73f12621345d6bb
farsan <apfarsan@gmail.com>
2023-06-15 17:09:50 +0530

    first

---
+++ src/classroom.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function classroom() {
+  return (
+    <div>This is a classroom</div>
+  )
+}
---
+++ src/office.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function office() {
+  return (
+    <div>this is a office</div>
+  )
+}
